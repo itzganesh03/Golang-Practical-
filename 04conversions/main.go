019e39a880abefbaf1ff5082c6fae0adf1f309b5
itzganesh <modiganesh83@gmail.com>
2022-09-08 01:35:02 +0530

    Conversation in Golang

---
+++ 04conversions/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to our Pizza App")
+	fmt.Println("Plz rate our Pizza Between 1 to 5")
+
+	reader := bufio.NewReader(os.Stdin)
+
+	input, _ := reader.ReadString('\n')
+
+	fmt.Println("Thanks for rating, ", input)
+
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to Your Rating: ", numRating+1)
+	}
+
+}
