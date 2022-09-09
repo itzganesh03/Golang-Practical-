1721f2ce3cd857168608f2293126fe2bfc809c07
itzganesh <modiganesh83@gmail.com>
2022-09-09 15:24:21 +0530

    Structs in Golang Syntax Update

--- 10mystructc/main.go
+++ 10mystructc/main.go
@@ -9,9 +9,10 @@ func main() {
 	//There is no inheritans in Golang
 	// Also No super and Parent Method in Golang
 
-	ganesh := User{"Ganesh", "ganesh@dev.go", true, 21}
+	ganesh := User{"Ganesh", "ganesh@go.dev", true, 21}
 	fmt.Println(ganesh)
 	fmt.Printf("User Detials: %+v\n", ganesh)
+	fmt.Printf("Name is %v and Email is %v", ganesh.Name, ganesh.Email)
 }
 
 type User struct {
