6f37ecb0b421a3d5473c00a255f3636440619e90
itzganesh <modiganesh83@gmail.com>
2022-09-13 15:06:19 +0530

    Update File Method in Golang

--- 17files/main.go
+++ 17files/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -13,14 +14,26 @@ func main() {
 	//Create Automatic file in folder and write to content
 	file, err := os.Create("./mygolangfile.txt")
 
-	if err != nil {
-		panic(err)
-	}
-
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkNilErr(err)
 	length, err := io.WriteString(file, content)
-	if err != nil {
-		panic(err)
-	}
+	checkNilErr(err)
 	fmt.Println("length is:", length)
 	defer file.Close()
+	readFile("./mygolangfile.txt")
+}
+
+// read method in txt file
+func readFile(filename string) {
+	databyte, err := ioutil.ReadFile(filename)
+	checkNilErr(err)
+	fmt.Println("Text data inside file is \n:", string(databyte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
