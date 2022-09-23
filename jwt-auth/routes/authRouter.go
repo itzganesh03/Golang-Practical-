63fcdc5d87eb9463ff375797c24511de6a0ac725
itzganesh <modiganesh83@gmail.com>
2022-09-23 15:22:56 +0530

    Revert "Revert "Authentication and Token generation Validation and Golang""
    This reverts commit 1476892c06d5e2a97223b80f1535c2028ba6f78e.

---
+++ jwt-auth/routes/authRouter.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	controller "github.com/itzganesh03/Golang-Practical-/jwtauth/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
