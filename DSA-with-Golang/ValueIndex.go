06dce03ae5f8c69747b057d2580788d0feb451b3
itzganesh <modiganesh83@gmail.com>
2022-09-30 00:06:59 +0530

    Value equal to index: Find the element whose value is equal to its index.

--- DSA-with-Golang/ValueIndex.go
+++ DSA-with-Golang/ValueIndex.go
@@ -1,10 +1,12 @@
 package main
+
 import "fmt"
+
 func main() {
-	a := []int{1,2,3,3,4}
-	for i:=range a{
-		if(a[i]==i){
-		fmt.Println("Found",i,"at index",a[i])
+	a := []int{1, 2, 3, 3, 4}
+	for i := range a {
+		if a[i] == i {
+			fmt.Println("Found", a[i], "at index", i)
 		}
 	}
 }
